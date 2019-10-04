60bd763677dfaedb5b95a4ab700e5e6e027ecd4c
Anis Mezdari <mezdari.anis@gmail.com>
2019-10-04 16:37:05 +0200

    add terrain

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,33 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <random>
+#include <time.h>  
+#include <chrono>
+#include <thread>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1000, 1000), "Jeu vidéo incroyable");
+	sf::RenderWindow window(sf::VideoMode(1250, 750), "Jeu vidéo incroyable");
 	
-	sf::CircleShape shape(100.f);
-	sf::CircleShape square(80.f, 4);
-	shape.setFillColor(sf::Color::Green);
-
+	srand(time(NULL));
+	sf::RectangleShape listRectangle[20][10];
+	sf::CircleShape shape(10.f);
+	shape.setFillColor(sf::Color(70, 130, 180));
+	shape.setPosition(15, 15);
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 10; j++) {
+			sf::RectangleShape  rectangle(sf::Vector2f(50.f, 50.f));
+			rectangle.setPosition(3 +( 50 * i) , 3 +(50 *j ));
+			rectangle.setOutlineThickness(2.f);
+			rectangle.setOutlineColor(sf::Color(0, 0, 0));
+			int randNum = std::rand() % 5 + 1;
+			if (randNum == 5) {
+				rectangle.setFillColor(sf::Color(0, 0, 0));
+			}
+			listRectangle[i][j] = rectangle;
+		}
+	}
+	int i = 1;
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -18,10 +38,17 @@ int main()
 		}
 
 		window.clear(sf::Color(220, 220, 220, 50));
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 10; j++) {
+				window.draw(listRectangle[i][j]);
+			}
+		}
 		window.draw(shape);
-		window.draw(square);
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		shape.setPosition(15 + (i * 50),15 + (i * 50));
+		i++;
 		window.display();
 	}
-
+	
 	return 0;
 }
